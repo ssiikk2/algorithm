8bf28fe4dc6ee533ee90cca8db027041803f2d51
Seongsik Ryu <siik0924@naver.com>
2022-01-29 22:49:45 +0900

    101010

--- src/main.java
+++ src/main.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class main {
+import java.util.*;
+
+public class main {
+    public static void main(String[] args){
+        Scanner n = new Scanner(System.in);
+        int val0 = n.nextInt();
+
+        int val1 =0;
+        int val2 =0;
+        int val3 =0;
+
+
+        if(val0 % 10 != 0){
+            System.out.println("-1");
+        }else {
+            while (val0 >= 10 && val0 <= 10000) {
+                if (val0 >= 300) {
+                    val0 -= 300;
+                    val1++;
+                } else if (val0 >= 60) {
+                    val0 -= 60;
+                    val2++;
+                } else if (val0 >= 10) {
+                    val0 -= 10;
+                    val3++;
+                }
+            }
+            System.out.println(val1+""+" "+val2+""+" "+val3);
+        }
+
+
+    }
 }
